001a5cc15e19ebf7ae9cf75f85b9d4db90b375c9
VitorDNJ <vitorjustino980@gmail.com>
2025-09-29 10:00:28 -0300

    adaptação do viacep
    ainda vou fazer as outras validações

---
+++ public/js/register.js
@@ -0,0 +1,14 @@
+   // Inicio do Viacep
+   
+   $('#cep').blur(function () {
+    var vl = this.value;
+
+    $.get('https://viacep.com.br/ws/'+vl+'/json/',function (dados) {
+        $('#endereco').val(dados.logradouro);
+        $('#bairro').val(dados.bairro);
+        $('#cidade').val(dados.localidade);
+        $('#estado').val(dados.uf);
+});
+});
+
+   // fim do Viacep
