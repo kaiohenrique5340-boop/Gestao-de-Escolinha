7c14b158b8cff122b696a6947ed34e9f48981b9d
Gabriel Avila <gabriel.bravin99@gmail.com>
2025-10-16 22:50:34 -0300

    Agora registra o ultimo login do usuario

--- login.php
+++ login.php
@@ -14,7 +14,6 @@ try {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-   
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
@@ -33,10 +32,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
  
     if ($usuario && password_verify($senha, $usuario['senha'])) {
+        date_default_timezone_set('America/Sao_Paulo');
+        $dataLogin = date('d/m/Y H:i:s');
+
+        $updateSql = "UPDATE usuarios SET data_ultimo_acesso = :data_ultimo_acesso WHERE id = :id";
+        $updateStmt = $pdo->prepare($updateSql);
+        $updateStmt->bindValue(':data_ultimo_acesso', $dataLogin);
+        $updateStmt->bindValue(':id', $usuario['id']);
+        $updateStmt->execute();
 
         $_SESSION['usuario_id'] = $usuario['id'];
         $_SESSION['usuario_email'] = $usuario['email'];
         $_SESSION['usuario_nome'] = $usuario['nome'];
+
         
 
         header("Location: painelAdministrador.html");
