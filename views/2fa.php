3214a5900efd898eb3bcb1237355426a8cfd4297
Gabriel Avila <gabriel.bravin99@gmail.com>
2025-10-29 17:29:40 -0300

    Reparo na logica de verificacao se o usuario tinha 2fa habilitado

---
+++ views/2fa.php
@@ -0,0 +1,44 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/esqueci_senha.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="shortcut icon" href="../img/—Pngtree—inspiration-boxing-logo-professional-boxer_5195569.ico" type="image/x-icon">
+    <title>Duplo fator de autenticação</title>
+</head>
+<body>
+    <header>
+        <a href="/">
+            <i class="bi bi-trophy"></i> Living <p>Fight</p>
+        </a>
+    </header>
+
+    <main>
+        <form id="form-reset" action="esqueci_senha.php" method="post">
+            <h2>Duplo fator de autenticação</h2> <br>
+            
+            <div id="mensagem"></div>
+
+            <input type="email" name="email" id="email" placeholder="Seu Email de Cadastro" required>
+            <br><br>
+            <input type="password" name="nova_senha" id="nova_senha" placeholder="Nova Senha" required>
+            <br><br>
+            <input type="password" name="confirma_senha" id="confirma_senha" placeholder="Confirme a Nova Senha" required>
+            <br><br>
+            <button type="submit">Redefinir</button>
+            <br><br>
+            <a href="login.php" class="register">Lembrou a senha? <span>Faça o login</span></a>
+        </form>
+    </main>
+
+    <script>
+    </script>
+</body>
+</html>
